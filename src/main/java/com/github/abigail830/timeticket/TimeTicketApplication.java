e569b7a81370522f80eff652abb3311036ee67fc
SaraQian <abigail830@163.com>
2019-04-05 15:17:28 +0800

    init commit

---
+++ src/main/java/com/github/abigail830/timeticket/TimeTicketApplication.java
@@ -0,0 +1,13 @@
+package com.github.abigail830.timeticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/abigail830/timeticket/TimeTicketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.abigail830.timeticket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TimeTicketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
